cba3d6bc467b9c937ec8d910221bdec55cc52cb7
kevinricaldez <kevinricaldez@grupotrc.com>
2025-06-02 16:35:00 +0200

    change docker

--- core/views.py
+++ core/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.views import View
 
+import psutil
+
 class HomeView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'home.html')
+        # Get system information
+        cpu_usage = psutil.cpu_percent(interval=1)
+        memory_info = psutil.virtual_memory()
+        memory_usage = memory_info.percent
+        disk_info = psutil.disk_usage('/')
+        disk_usage = disk_info.percent
+        total_disk_space = round(disk_info.total / (1024 ** 3), 2)  # Total disk space in GB
+        free_disk_space = round(disk_info.free / (1024 ** 3), 2)  # Total disk space in GB
+        # Pass the information to the template
+        context = {
+            'cpu_usage': cpu_usage,
+            'memory_usage': memory_usage,
+            'disk_usage': disk_usage,
+            'total_disk_space': total_disk_space,
+            'free_disk_space': free_disk_space,
+        }
+        return render(request, 'home.html', context)
--- django_task_manager/settings.py
+++ django_task_manager/settings.py
@@ -16,6 +16,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+HOST_PROC = os.getenv('HOST_PROC', '/proc')
+
+with open(f"{HOST_PROC}/meminfo") as f:
+    meminfo = f.read()
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
