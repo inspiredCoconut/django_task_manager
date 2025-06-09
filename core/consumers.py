69e926c9087d00bda7555cdf6e0a31c33d27e8a5
kevinricaldez <kevinricaldez@grupotrc.com>
2025-06-09 16:07:18 +0200

    mounted host disk for read only

--- core/consumers.py
+++ core/consumers.py
@@ -1,9 +1,11 @@
+import os
 import json
 import asyncio
 import psutil
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class StatsConsumer(AsyncWebsocketConsumer):
+    HOST_ROOT = os.getenv('HOST_ROOT', '/host_root')
     async def connect(self):
         await self.accept()
         self.running = True
@@ -17,7 +19,7 @@ class StatsConsumer(AsyncWebsocketConsumer):
             cpu_usage = psutil.cpu_percent(interval=1)
             memory_info = psutil.virtual_memory()
             memory_usage = memory_info.percent
-            disk_info = psutil.disk_usage('/')
+            disk_info = psutil.disk_usage(self.HOST_ROOT)
             disk_usage = disk_info.percent
             total_disk_space = round(disk_info.total / (1024 ** 3), 2)  # Total disk space in GB
             free_disk_space = round(disk_info.free / (1024 ** 3), 2)  # Total disk space in GB
